03e56c6253c8425d35897e3bc7a50f870088a391
taewon-ted <taewon.ted@navercorp.com>
2017-12-19 11:31:58 +0900

    Add Follow logic

--- app/src/main/java/taewon/navercorp/integratedsns/util/TwitchLoginActivity.java
+++ app/src/main/java/taewon/navercorp/integratedsns/util/TwitchLoginActivity.java
@@ -31,7 +31,7 @@ public class TwitchLoginActivity extends AppCompatActivity {
     private void initView() {
         webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
-        Log.d("FAVO_TEST", "Check branch");
+        Log.d("FAVO_TEST", "Check branch, Follow test");
     }
 
     private void initData(){
